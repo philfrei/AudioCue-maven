958763761206924538de5798a7b6086dc42b70a2
philfrei <philfrei@aol.com>
2022-03-20 20:13:24 -0700

    FIXUP Java JVM and testing dependencies

--- src/test/java/com/adonax/audiocue/AudioCueTest.java
+++ src/test/java/com/adonax/audiocue/AudioCueTest.java
@@ -1,14 +1,13 @@
 package com.adonax.audiocue;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class AudioCueTest {
 
 	@Test
 	void test() {
-		assertEquals(0, 0);
+		Assertions.assertEquals(0, 0);
 	}
 
 }
